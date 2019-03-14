099e4d88a33d2fc7fe70352f1665f94ecdc48bbd
Logon Aluno <logonrmlocal@fiap.com.br>
2019-03-14 20:44:26 -0300

    JUNIT refatorado ao Design Patterns

--- 05-JPA-Design-Patterns/src/br/com/fiap/teste/MusicaDAOTeste.java
+++ 05-JPA-Design-Patterns/src/br/com/fiap/teste/MusicaDAOTeste.java
@@ -6,6 +6,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.fiap.dao.impl.MusicaDAOImpl;
@@ -15,15 +17,24 @@ import br.com.fiap.exception.ChaveInexistenteException;
 import br.com.fiap.exception.CommitException;
 
 class MusicaDAOTeste {
-
-	@Test
-	void buscar() {
-
+	
+	private static MusicaDAOImpl dao;
+	private Musica m;
+	
+	//Método static que será executado antes de todos os testes
+	@BeforeAll
+	public static void init() {
 		// Arrange - instanciar os objetos
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("teste");
 		EntityManager em = emf.createEntityManager();
-		MusicaDAOImpl dao = new MusicaDAOImpl(em);
-		Musica m = new Musica("Teste", 4, Genero.AXE);
+		dao = new MusicaDAOImpl(em);
+	}
+	
+	//Método que será executado antes de cada teste
+	@BeforeEach
+	public void antesDosTestes() {
+		// Arrange - instanciar os objetos
+		m = new Musica("Teste", 4, Genero.AXE);
 
 		// Act - chamar o método que será testado
 		try {
@@ -33,11 +44,53 @@ class MusicaDAOTeste {
 			e.printStackTrace();
 			fail("Falha na inserção de dado");
 		}
+	}
+	
+	@Test
+	void atualizar() {	
+		//Passando um valor de atualização
+		m.setTipo(Genero.FORRO);
 		
+		//Atualizar
+		try {
+			dao.atualizar(m);
+			dao.commit();
+		} catch (CommitException e) {
+			e.printStackTrace();
+			fail("Falha ao commitar atualização");
+		}
+		
+		//Buscar
+		try {
+			Musica musica = dao.consultar(m.getCodigo());
+			assertEquals(Genero.FORRO, musica.getTipo());
+		} catch (ChaveInexistenteException e) {
+			e.printStackTrace();
+			fail("Falha na busca");
+		}
+	}
+	
+	@Test
+	void remover() {		
+		try {
+			Musica musica = dao.consultar(m.getCodigo());
+			dao.remover(musica.getCodigo());
+			dao.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Falha na busca");
+		}
+		
+		assertThrows(ChaveInexistenteException.class, ()-> dao.consultar(m.getCodigo()));
+	}
+	
+	@Test
+	void buscar() {		
 		try {
 			Musica musica = dao.consultar(m.getCodigo());
 			// Assert - validação do resultado
 			assertNotNull(musica);
+			assertEquals("Teste", musica.getNome());
 		} catch (ChaveInexistenteException e) {
 			e.printStackTrace();
 			fail("Falha na busca");
@@ -46,22 +99,6 @@ class MusicaDAOTeste {
 
 	@Test
 	void cadastrar() {
-
-		// Arrange - instanciar os objetos
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("teste");
-		EntityManager em = emf.createEntityManager();
-		MusicaDAOImpl dao = new MusicaDAOImpl(em);
-		Musica m = new Musica("Teste", 4, Genero.AXE);
-
-		// Act - chamar o método que será testado
-		try {
-			dao.cadastrar(m);
-			dao.commit();
-		} catch (CommitException e) {
-			e.printStackTrace();
-			fail("Falha na inserção de dado");
-		}
-
 		// Assert - validação do resultado
 		// valida se a sequence gerou um código
 		assertNotEquals(0, m.getCodigo());
